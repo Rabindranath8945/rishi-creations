52a091b41f1ac43ac881b31a4d9ceec1b7dd3a41
rabindranath8945 <connectrnmwd@gmail.com>
2024-07-21 07:19:25 +0530

    Second update

---
+++ src/app/components/About.js
@@ -0,0 +1,38 @@
+export default function About() {
+  return (
+    <>
+      <section id="about" className="about">
+        <div className="container">
+          <div className="about-features-content">
+            <div className="about-content-inner">
+              <h2 className="about-content-heading">Campaign Tools</h2>
+              <p className="about-para">
+                Dissuade ecstatic and properly saw entirely sir why laughter
+                endeavor. In on my jointure horrible margaret suitable.
+              </p>
+            </div>
+            <div className="about-content-inner">
+              <h2 className="about-content-heading">Campaign Tools</h2>
+              <p className="about-para">
+                Dissuade ecstatic and properly saw entirely sir why laughter
+                endeavor. In on my jointure horrible margaret suitable.
+              </p>
+            </div>
+            <div className="about-content-inner">
+              <h2 className="about-content-heading">Campaign Tools</h2>
+              <p className="about-para">
+                Dissuade ecstatic and properly saw entirely sir why laughter
+                endeavor. In on my jointure horrible margaret suitable.
+              </p>
+            </div>
+          </div>
+          <div className="about-main-content">
+            <div className="about-main-content-inner">
+              <p>Hello World</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,10 +1,9 @@
 import Image from "next/image";
-// import Link from "next/link";
 
 export default function Header() {
   return (
     <>
-      <section className="header">
+      <section id="home" className="header">
         <div className="container-fluid">
           <div className="header-inner">
             <div className="header-logo">
@@ -13,31 +12,81 @@ export default function Header() {
             <div className="header-menu">
               <ul className="header-menu-list">
                 <li className="header-menu-list-item">
-                  <a className="header-menu-list-item-link" href="/about">
+                  <a className="header-menu-list-item-link" href="#about">
                     About
                   </a>
                 </li>
                 <li className="header-menu-list-item">
-                  <a className="header-menu-list-item-link" href="/offer">
+                  <a className="header-menu-list-item-link" href="#offer">
                     Offer
                   </a>
                 </li>
                 <li className="header-menu-list-item">
-                  <a className="header-menu-list-item-link" href="/project">
+                  <a className="header-menu-list-item-link" href="#pricing">
                     Pricing
                   </a>
                 </li>
                 <li className="header-menu-list-item">
-                  <a className="header-menu-list-item-link" href="/project">
+                  <a className="header-menu-list-item-link" href="#template">
                     Templates
                   </a>
                 </li>
                 <li className="header-menu-list-item">
-                  <a className="header-menu-list-item-link" href="/contact">
+                  <a className="header-menu-list-item-link" href="#contact">
                     Contact Us
                   </a>
                 </li>
               </ul>
+            </div>
+            <div className="header-sub-menu">
+              <ul className="header-menu-list">
+                <li className="header-menu-list-item">
+                  <a
+                    className="header-menu-list-btn header-menu-primary-btn"
+                    href="/register"
+                  >
+                    Register
+                  </a>
+                </li>
+                <li className="header-menu-list-item">
+                  <a
+                    className="header-menu-list-btn header-menu-outline-btn"
+                    href="/login"
+                  >
+                    Login
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="banner">
+            <div className="banner-inner">
+              <div className="banner-inner-content">
+                <h1 className="banner-first-heading">Discover the world</h1>
+                <h2 className="banner-second-heading">
+                  with
+                  <span className="banner-second-heading-span">
+                    digital marketplace
+                  </span>
+                </h2>
+                <div className="banner-inner-content-desc">
+                  <p>
+                    Dissuade ecstatic and properly saw entirely sir why laughter
+                    <br />
+                    endeavor. In on my jointure horrible margaret suitable.
+                  </p>
+                </div>
+                <div className="banner-inner-content-desc-btn">
+                  <a
+                    className="banner-inner-content-desc-btn-item"
+                    href="#template"
+                  >
+                    Get Started
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
